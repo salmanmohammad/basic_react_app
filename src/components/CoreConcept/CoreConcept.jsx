8cc9aeff1575f9186c67f1634bea7bf3d6b6748d
salmanmohammad <salmanmohammad432@gmail.com>
2024-01-05 19:42:32 +0530

    CoreConcept component added

---
+++ src/components/CoreConcept/CoreConcept.jsx
@@ -0,0 +1,12 @@
+import './CoreConcept.css';
+
+export default function CoreConcept(props)
+{
+  return(
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
